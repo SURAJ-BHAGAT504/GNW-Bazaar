2ff44a40f52ad8396f982f2f0a8e58e4c2aa589a
SURAJ-BHAGAT504 <surajbhagat504@gmail.com>
2025-08-26 19:50:54 +0530

    Api Gatwway has implemented for Health care category Api and basic changes

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+app.MapControllers();
+await app.UseOcelot();
+
+app.Run();
